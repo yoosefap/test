f435b40736bddcc7802550879cd8b73ce3771651
yoosefap <yoosefalipour@gmail.com>
2024-07-31 17:31:54 +0330

    Add dist

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
 
     <div class="number-page">
-       <div>
-           <h1>تبدیل اعداد به حروف <i>(آلمانی)</i></h1>
-       </div>
+        <div>
+            <h1>تبدیل اعداد به حروف <i>(آلمانی)</i></h1>
+        </div>
 
         <div class="container">
-            <input type="text" inputmode="number" placeholder="عدد رو وارد کنید" v-model="inputNumber"/>
+            <input type="text" inputmode="number"
+                   placeholder="عدد رو وارد کنید"
+                   v-model="formattedNumber"/>
         </div>
     </div>
 
     <div class="container">
-            <p v-if="inputNumber" class="result">{{ germanWords }}</p>
+        <p v-if="inputNumber" class="result">{{ germanWords }}</p>
     </div>
 
 
@@ -26,12 +28,22 @@ const germanWords = computed(() => {
     return numberToGermanWords(inputNumber.value);
 })
 
+const formattedNumber = computed({
+    get() {
+        return numFormat(inputNumber.value);
+    },
+    set(value) {
+        inputNumber.value = value.replace(/[^\d]/g, '');
+    }
+});
+
 function numberToGermanWords(num) {
     const units = ["", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun"];
     const teens = ["zehn", "elf", "zwölf", "dreizehn", "vierzehn", "fünfzehn", "sechzehn", "siebzehn", "achtzehn", "neunzehn"];
     const tens = ["", "", "zwanzig", "dreißig", "vierzig", "fünfzig", "sechzig", "siebzig", "achtzig", "neunzig"];
     const scales = ["", "tausend", "Millionen", "Milliarde", "Billionen", "Billiarde", "Trillionen", "Trilliarde"];
 
+    num = parseInt(num.toString().replace(/,/g, ""), 0);
     if (num === 0) return "null";
 
     function getHundreds(n) {
@@ -95,6 +107,51 @@ function numberToGermanWords(num) {
     return words.trim();
 }
 
+
+const numFormat = (num) => {
+    num = persianToEnglishNumbers(num);
+    if (num == null) return;
+    let minus = (num < 0);
+    if (minus)
+        num = num * -1;
+
+    let str = num.toString().replace("$", ""), parts = false, output = [], i = 1, formatted = null;
+    if (str.indexOf(".") > 0) {
+        parts = str.split(".");
+        str = parts[0];
+    }
+    str = str.split("").reverse();
+    for (var j = 0, len = str.length; j < len; j++) {
+        if (str[j] != ",") {
+            output.push(str[j]);
+            if (i % 3 == 0 && j < (len - 1)) {
+                output.push(",");
+            }
+            i++;
+        }
+    }
+    formatted = output.reverse().join("");
+    num = ("" + formatted + ((parts) ? "." + parts[1].substr(0, 2) : ""));
+    return (!minus) ? num : '-' + num;
+};
+
+const persianToEnglishNumbers = (input) => {
+    if (typeof input !== 'string') return input;
+    const mapping = {
+        '۰': '0',
+        '۱': '1',
+        '۲': '2',
+        '۳': '3',
+        '۴': '4',
+        '۵': '5',
+        '۶': '6',
+        '۷': '7',
+        '۸': '8',
+        '۹': '9',
+    };
+
+    return input.replace(/[۰-۹]/g, (match) => mapping[match]);
+};
 </script>
 
 
@@ -105,7 +162,7 @@ function numberToGermanWords(num) {
     font-family: Arial, sans-serif;
 }
 
-.number-page{
+.number-page {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -122,7 +179,7 @@ i {
     font-size: 1rem;
 }
 
-.container{
+.container {
     display: block;
 }
 
